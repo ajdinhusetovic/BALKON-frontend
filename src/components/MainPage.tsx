221f21846ba8900398744355112048ffe82a5a21
Ajdin Husetovic <ahusetovic1@gmail.com>
2024-11-08 14:09:28 +0100

    ui fix

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -6,11 +6,11 @@ const MainPage = () => {
     <div>
       <div className="flex flex-col items-center mt-16 mb-16 lg:mb-0 lg:flex-row gap-16 lg:mt-32 justify-center h-full">
         <div className="pt-8 lg:w-[40%] w-10/12">
-          <h1 className="p-2 bg-light-brown-color w-fit mb-2 rounded-2xl font-semibold text-white">books.</h1>
+          <h1 className="p-2 bg-light-brown-color w-fit mb-2 rounded-2xl font-semibold text-white">books</h1>
           <BookTable />
         </div>
         <div className="pt-8 lg:w-[40%] w-10/12">
-          <h1 className="p-2 bg-light-brown-color w-fit mb-2 rounded-2xl font-semibold text-white">authors.</h1>
+          <h1 className="p-2 bg-light-brown-color w-fit mb-2 rounded-2xl font-semibold text-white">authors</h1>
           <AuthorTable />
         </div>
       </div>
